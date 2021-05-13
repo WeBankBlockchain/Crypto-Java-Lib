04e208b8013ce34d7f9601bd3763456e4ff58cae
MaggieNgWu <hao.maggie.wu@gmail.com>
2021-05-13 11:08:08 +0800

    vrf functions support linux x86, linux ARM, MacOS, Windows.

--- java-crypto-core/src/main/java/com/webank/wedpr/crypto/NativeInterface.java
+++ java-crypto-core/src/main/java/com/webank/wedpr/crypto/NativeInterface.java
@@ -2,19 +2,19 @@ package com.webank.wedpr.crypto;
 
 import com.webank.wedpr.utils.EnvironmentUtils;
 import com.webank.wedpr.utils.NativeUtils;
-
 import java.io.IOException;
 
 /**
  * Native interface for signature
- * @author aaronchu
- * @Description
+ *
+ * @author aaronchu @Description
  * @data 2020/06/18
  */
 public class NativeInterface {
 
     private static final String LIB_FFI_RESOURCE_PATH;
     private static final String LIB_FFI_NAME = "WeDPR_dynamic_lib/libffi_java_sdk";
+
     static {
         String os = System.getProperty("os.name");
         String tail = EnvironmentUtils.getResourceTailByOs(os);
@@ -38,7 +38,8 @@ public class NativeInterface {
 
     public static native CryptoResult secp256k1Sign(String priKey, String messageHash);
 
-    public static native CryptoResult secp256k1Verify(String pubKey, String message, String signature);
+    public static native CryptoResult secp256k1Verify(
+            String pubKey, String message, String signature);
 
     public static native CryptoResult secp256k1RecoverPublicKey(String message, String signature);
 
@@ -53,14 +54,16 @@ public class NativeInterface {
     public static native CryptoResult sm2SignFast(String priKey, String pubKey, String messageHash);
 
     public static native CryptoResult sm2Verify(String pubKey, String message, String signature);
-    
+
     public static native CryptoResult sm2ComputeHashE(String pubKey, String sm2ComputeE);
 
     public static native CryptoResult curve25519VrfProveUtf8(String privateKey, String utf8Message);
 
-    public static native CryptoResult curve25519VrfProveFastUtf8(String privateKey, String publicKey, String utf8Message);
+    public static native CryptoResult curve25519VrfProveFastUtf8(
+            String privateKey, String publicKey, String utf8Message);
 
-    public static native CryptoResult curve25519VrfVerifyUtf8(String publicKey, String utf8Message, String proof);
+    public static native CryptoResult curve25519VrfVerifyUtf8(
+            String publicKey, String utf8Message, String proof);
 
     public static native CryptoResult curve25519VrfDerivePublicKey(String privateKey);
 
@@ -68,25 +71,20 @@ public class NativeInterface {
 
     public static native CryptoResult curve25519VrfIsValidPublicKey(String publicKey);
 
-
-
-    private static String resolveLibTail(String os){
+    private static String resolveLibTail(String os) {
         os = os.toLowerCase();
         String tail;
-        if(os.contains("windows")){
+        if (os.contains("windows")) {
             tail = ".dll";
-        }
-        else if(os.contains("linux")){
+        } else if (os.contains("linux")) {
             tail = ".so";
-        }
-        else if(os.contains("mac")){
+        } else if (os.contains("mac")) {
             tail = ".dylib";
-        }
-        else{
-            throw new RuntimeException("Unsupported os: "+os);
+        } else {
+            throw new RuntimeException("Unsupported os: " + os);
         }
         return tail;
     }
 
-    private NativeInterface(){}
+    private NativeInterface() {}
 }
